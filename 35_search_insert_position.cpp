6d0381bdc299501e267ab995119776e5ced1cb53
huzeyfecelik <huzeyfecelik34@gmail.com>
2025-02-26 02:43:22 +0300

    solve search insert position in c++

---
+++ 35_search_insert_position.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = 0;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        nums.push_back(target);
+        std::ranges::sort(nums);
+        return this->searchInsert(nums, target);
+    }
+};
